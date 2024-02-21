5c808f9045919466ed2c7b890b2e01e5a3aa8de7
alaa wahba <alaawahbaa13@gmail.com>
2024-02-21 22:17:02 +0200

    Time/Clock Category

---
+++ blockly/blocks/arduino/time.js
@@ -0,0 +1,97 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Blocks for Arduino Time functions.
+ *     The arduino built in functions syntax can be found in
+ *     http://arduino.cc/en/Reference/HomePage
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.time');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly.Types');
+
+
+/** Common HSV hue for all blocks in this category. */
+Blockly.Blocks.time.HUE = 140;
+
+Blockly.Blocks['time_delay'] = {
+  /**
+   * Delay block definition
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendValueInput('DELAY_TIME_MILI')
+        .setCheck(Blockly.Types.NUMBER.checkList)
+        .appendField(Blockly.Msg.ARD_TIME_DELAY);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_TIME_MS);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.ARD_TIME_DELAY_TIP);
+  }
+};
+
+
+Blockly.Blocks['infinite_loop'] = {
+  /**
+   * Waits forever, end of program.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('');
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.ARD_TIME_INF);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setTooltip(Blockly.Msg.ARD_TIME_INF_TIP);
+  }
+};
+
+//New Blocks
+
+Blockly.Blocks['time_clockEN'] = {
+  init: function() {
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.CLK_EN)
+        .appendField(new Blockly.FieldDropdown(
+            Blockly.Arduino.Boards.selected.peripherals), 'PERIPHRAL')
+        .appendField(Blockly.Msg.CLK_FOR)
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.CLK_EN_TIP);
+  },
+  
+};
+Blockly.Blocks['time_clockEN'].getBlockType = function() {
+  return Blockly.Types.BOOLEAN;
+};
+
+Blockly.Blocks['time_clockDisable'] = {
+  init: function() {
+    this.setColour(Blockly.Blocks.time.HUE);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.CLK_DIS)
+        .appendField(new Blockly.FieldDropdown(
+            Blockly.Arduino.Boards.selected.peripherals), 'PERIPHRAL')
+        .appendField(Blockly.Msg.CLK_FOR)
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.CLK_DIS_TIP);
+  },
+  
+};
+Blockly.Blocks['time_clockDisable'].getBlockType = function() {
+  return Blockly.Types.BOOLEAN;
+};
+
---
+++ blockly/generators/arduino/time.js
@@ -0,0 +1,65 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Arduino code generator for the Time blocks.
+ *     Arduino built-in function docs: http://arduino.cc/en/Reference/HomePage
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.time');
+
+goog.require('Blockly.Arduino');
+
+
+/**
+ * Code generator for the delay Arduino block.
+ * Arduino code: loop { delay(X); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+ Blockly.Arduino['time_delay'] = function(block) {
+  var delayTime = Blockly.Arduino.valueToCode(
+      block, 'DELAY_TIME_MILI', Blockly.Arduino.ORDER_ATOMIC) || '0';
+  var code = 'delay_ms(' + delayTime + ');\n';
+  return code;
+};
+
+
+
+/**
+ * Code generator for the wait forever (end of program) block
+ * Arduino code: loop { while(true); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+ Blockly.Arduino['infinite_loop'] = function(block) {
+  return 'while(true);\n';
+};
+
+// New block 
+/**
+ * 
+ */
+Blockly.Arduino['time_clockEN'] = function(block) {
+  var peripheralID = block.getFieldValue('PERIPHRAL');
+  var busID= 'APB2_ID';
+  if((peripheralID == 'USART2') || (peripheralID == 'USART3') || (peripheralID == 'SPI2') || (peripheralID == 'I2C2')||(peripheralID == 'I2C1'))
+      busID = 'APB1_ID'
+  var pinMainCode = '\nRCC_CLK_EN(' + busID + ','+ peripheralID+ '_ID);\n';
+  Blockly.Arduino.addMain('time_' , pinMainCode, false);
+  return "";
+};
+
+
+Blockly.Arduino['time_clockDisable'] = function(block) {
+  var peripheralID = block.getFieldValue('PERIPHRAL');
+  var busID= 'APB2_ID';
+  if((peripheralID == 'USART2') || (peripheralID == 'USART3') || (peripheralID == 'SPI2') || (peripheralID == 'I2C2')||(peripheralID == 'I2C1'))
+      busID = 'APB1_ID'
+  var Code = 'RCC_CLK_RST(' + busID + ','+ peripheralID+ '_ID);\n';
+  Blockly.Arduino.addMain('time' , Code, false);
+  return "";
+};
