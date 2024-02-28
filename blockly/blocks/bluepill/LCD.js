abffaf5c10cb2080e732e925e38cc370340e5101
alaa wahba <alaawahbaa13@gmail.com>
2024-02-28 21:45:33 +0200

    adding LCD configuration

--- blockly/blocks/bluepill/LCD.js
+++ blockly/blocks/bluepill/LCD.js
@@ -1,91 +1,113 @@
+"use strict";
 
-'use strict';
+goog.provide("Blockly.Blocks.lcd");
 
-goog.provide('Blockly.Blocks.lcd');
-
-goog.require('Blockly.Blocks');
-goog.require('Blockly.Types');
+goog.require("Blockly.Blocks");
+goog.require("Blockly.Types");
 
 /** Common HSV hue for all blocks in this category. */
 Blockly.Blocks.lcd.HUE = 400;
 
-Blockly.Blocks['lcd_init'] = {
-
-  init: function() {
+Blockly.Blocks["lcd_init"] = {
+  init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
     this.appendDummyInput()
-        .appendField(Blockly.Msg.LCD_INIT)
-        .appendField(new Blockly.FieldDropdown(
-            Blockly.Arduino.Boards.selected.full_ports), 'PORT');
+      .appendField(Blockly.Msg.LCD_INIT)
+      .appendField(Blockly.Msg.LCD_SIZE)
+      .appendField(
+        new Blockly.FieldDropdown([["2x16"], ["4x16"], ["4x20"]]),
+        "SIZE"
+      )
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.full_ports),
+        "PORT"
+      )
+      .appendField(Blockly.Msg.LCD_D4)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "d4"
+      )
+      .appendField(Blockly.Msg.LCD_D5)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "d5"
+      )
+      .appendField(Blockly.Msg.LCD_D6)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "d6"
+      );
+    this.appendDummyInput()
+      .appendField(Blockly.Msg.LCD_D7)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "d7"
+      )
+      .appendField(Blockly.Msg.LCD_EN)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "EN"
+      )
+      .appendField(Blockly.Msg.LCD_RS)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "RS"
+      );
+
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip(Blockly.Msg.LCD_INIT_TTL);
   },
-  
 };
 
-
-
-// send char 
-Blockly.Blocks['lcd_sendChar'] = {
- 
-  init: function() {
-   this.setColour(Blockly.Blocks.lcd.HUE);
-   this.appendValueInput('DATA')
-       .appendField(Blockly.Msg.LCD_CHAR);
-   this.setInputsInline(true);
-   this.setPreviousStatement(true, null);
-   this.setNextStatement(true, null);
-   this.setTooltip(Blockly.Msg.LCD_CHAR_TTL);
- }
-  
- };
-
-
-// send string 
-Blockly.Blocks['lcd_sendString'] = {
- 
-     init: function() {
-      this.setColour(Blockly.Blocks.lcd.HUE);
-      this.appendValueInput('DATA')
-          .setCheck(Blockly.Types.TEXT.checkList)     
-          .appendField(Blockly.Msg.LCD_STRING);
-      this.setInputsInline(true);
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setTooltip(Blockly.Msg.LCD_STRING_TTL);
-    }
-     
-    };
-
-// clear screen 
-Blockly.Blocks['lcd_clear'] = {
-
-  init: function() {
+// send char
+Blockly.Blocks["lcd_sendChar"] = {
+  init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.LCD_CLEAR)
+    this.appendValueInput("DATA").appendField(Blockly.Msg.LCD_CHAR);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.LCD_CHAR_TTL);
+  },
+};
+
+// send string
+Blockly.Blocks["lcd_sendString"] = {
+  init: function () {
+    this.setColour(Blockly.Blocks.lcd.HUE);
+    this.appendValueInput("DATA")
+      .setCheck(Blockly.Types.TEXT.checkList)
+      .appendField(Blockly.Msg.LCD_STRING);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip(Blockly.Msg.LCD_STRING_TTL);
+  },
+};
+
+// clear screen
+Blockly.Blocks["lcd_clear"] = {
+  init: function () {
+    this.setColour(Blockly.Blocks.lcd.HUE);
+    this.appendDummyInput().appendField(Blockly.Msg.LCD_CLEAR);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
   },
-  
 };
 
-// go to x y 
-Blockly.Blocks['lcd_goto'] = {
-  init: function() {
+// go to x y
+Blockly.Blocks["lcd_goto"] = {
+  init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendValueInput('ROW')
-        .setCheck(Blockly.Types.NUMBER.checkList)
-        .appendField(Blockly.Msg.LCD_GOTOx);
-    this.appendDummyInput()
-        .appendField(Blockly.Msg.LCD_GOTOy);
-    this.appendValueInput('COL')
-        .setCheck(Blockly.Types.NUMBER.checkList)
+    this.appendValueInput("ROW")
+      .setCheck(Blockly.Types.NUMBER.checkList)
+      .appendField(Blockly.Msg.LCD_GOTOx);
+    this.appendDummyInput().appendField(Blockly.Msg.LCD_GOTOy);
+    this.appendValueInput("COL").setCheck(Blockly.Types.NUMBER.checkList);
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip(Blockly.Msg.LCD_GOTO_TTL);
-  }
-  
+  },
 };
--- blockly/generators/bluepill/LCD.js
+++ blockly/generators/bluepill/LCD.js
@@ -1,74 +1,105 @@
-
 /**
  * @fileoverview Code generator for Keypad in HAL layer
- *    
+ *
  */
-'use strict';
+"use strict";
 
-goog.provide('Blockly.Arduino.lcd');
+goog.provide("Blockly.Arduino.lcd");
 
-goog.require('Blockly.Arduino');
+goog.require("Blockly.Arduino");
 
+Blockly.Arduino["lcd_init"] = function (block) {
+  var port = block.getFieldValue("PORT");
+  var gpio = port.charAt(4);
+  var D4 = "PIN_" + block.getFieldValue("d4").charAt(2);
+  var D5 = "PIN_" + block.getFieldValue("d5").charAt(2);
+  var D6 = "PIN_" + block.getFieldValue("d6").charAt(2);
+  var D7 = "PIN_" + block.getFieldValue("d7").charAt(2);
+  var RS = "PIN_" + block.getFieldValue("RS").charAt(2);
+  var EN = "PIN_" + block.getFieldValue("EN").charAt(2);
+  var SIZE = "LCD_" + block.getFieldValue("SIZE");
 
-Blockly.Arduino['lcd_init'] = function(block) {
-    var port = block.getFieldValue('PORT');
-    var gpio = port.charAt(4); 
-
-    console.log(port); 
-   for(var i =4 ;i<8 ; i++){
-      var pin = gpio + i;
-      Blockly.Arduino.reservePin(
-         block, pin, Blockly.Arduino.PinTypes.INPUT, 'keypad pins');
-      }
-      pin = 'P'+gpio +'2';
-      Blockly.Arduino.reservePin(
-        block, pin, Blockly.Arduino.PinTypes.INPUT, 'keypad pins');
-      
-        pin = 'P'+gpio +'3';
-        Blockly.Arduino.reservePin(
-          block, pin, Blockly.Arduino.PinTypes.INPUT, 'keypad pins');
-          
-      editFileAtLine("STMCubeProject/HAL/LCD/LCD.h", 12, "#define LCD_PORT     "+port);
-
-  var pinMainCode = 'lcd_init();\n';
-  Blockly.Arduino.addMain('lcd_' + port, pinMainCode, false);
+  for (var i = 4; i < 8; i++) {
+    var pin = block.getFieldValue("d" + i);
+    Blockly.Arduino.reservePin(
+      block,
+      pin,
+      Blockly.Arduino.PinTypes.INPUT,
+      "LCD pins"
+    );
+  }
+  pin = block.getFieldValue("RS");
+  Blockly.Arduino.reservePin(
+    block,
+    pin,
+    Blockly.Arduino.PinTypes.INPUT,
+    "LCD pins"
+  );
+  pin = block.getFieldValue("EN" + i);
+  Blockly.Arduino.reservePin(
+    block,
+    pin,
+    Blockly.Arduino.PinTypes.INPUT,
+    "LCD pins"
+  );
+  var pinMainCode =
+    "LCD_t LCD_Config= {" +
+    SIZE +
+    "," +
+    port +
+    "," +
+    port +
+    "," +
+    EN +
+    "," +
+    RS +
+    "," +
+    D4 +
+    "," +
+    D5 +
+    "," +
+    D6 +
+    "," +
+    D7 +
+    "};\nlcd_init(&LCD_Config);\n";
+  Blockly.Arduino.addMain("lcd_" + port, pinMainCode, false);
 
   return "";
-
 };
- //send char 
+//send char
 
- Blockly.Arduino['lcd_sendChar'] = function(block) {
-  var data = Blockly.Arduino.valueToCode(
-    block, 'DATA', Blockly.Arduino.ORDER_ATOMIC) || '0';  
-   var code = 'lcd_Send_Char('+ data +');\n';
+Blockly.Arduino["lcd_sendChar"] = function (block) {
+  var data =
+    Blockly.Arduino.valueToCode(block, "DATA", Blockly.Arduino.ORDER_ATOMIC) ||
+    "0";
+  var code = "lcd_Send_Char(" + data + ");\n";
   return code;
- };
-// send string 
+};
+// send string
 
-
-Blockly.Arduino['lcd_sendString'] = function(block) {
-  var data = Blockly.Arduino.valueToCode(
-    block, 'DATA', Blockly.Arduino.ORDER_ATOMIC) || '0';  
-   var code = 'lcd_send_String('+ data +');\n';
+Blockly.Arduino["lcd_sendString"] = function (block) {
+  var data =
+    Blockly.Arduino.valueToCode(block, "DATA", Blockly.Arduino.ORDER_ATOMIC) ||
+    "0";
+  var code = "lcd_send_String(" + data + ");\n";
   return code;
- };
-
-
-// clear screen 
-
- Blockly.Arduino['lcd_clear'] = function(block) {
-    return 'lcd_Clear_Screen();\n';
 };
 
-// go to x , y 
-Blockly.Arduino['lcd_goto'] = function(block) {
-  var x = Blockly.Arduino.valueToCode(
-    block, 'ROW', Blockly.Arduino.ORDER_ATOMIC) || '0';  
-  var y= Blockly.Arduino.valueToCode(
-      block, 'COL', Blockly.Arduino.ORDER_ATOMIC) || '0';  
-  
- var code = 'lcd_GOTO_XY('+ x + ','+y +');\n';
- return code;
+// clear screen
 
+Blockly.Arduino["lcd_clear"] = function (block) {
+  return "lcd_Clear_Screen();\n";
+};
+
+// go to x , y
+Blockly.Arduino["lcd_goto"] = function (block) {
+  var x =
+    Blockly.Arduino.valueToCode(block, "ROW", Blockly.Arduino.ORDER_ATOMIC) ||
+    "0";
+  var y =
+    Blockly.Arduino.valueToCode(block, "COL", Blockly.Arduino.ORDER_ATOMIC) ||
+    "0";
+
+  var code = "lcd_GOTO_XY(" + x + "," + y + ");\n";
+  return code;
 };
