aa38ae68bde2554cece3a5b9de1b5c0d8ee2d54f
alaa wahba <alaawahbaa13@gmail.com>
2024-02-28 21:45:04 +0200

    Adding keypad configuration

--- blockly/blocks/bluepill/Keypad.js
+++ blockly/blocks/bluepill/Keypad.js
@@ -1,44 +1,73 @@
+"use strict";
 
-'use strict';
+goog.provide("Blockly.Blocks.keypad");
 
-goog.provide('Blockly.Blocks.keypad');
-
-goog.require('Blockly.Blocks');
-goog.require('Blockly.Types');
+goog.require("Blockly.Blocks");
+goog.require("Blockly.Types");
 
 /** Common HSV hue for all blocks in this category. */
 Blockly.Blocks.keypad.HUE = 380;
 
-Blockly.Blocks['keypad_init'] = {
-
-  init: function() {
+Blockly.Blocks["keypad_init"] = {
+  init: function () {
     this.setColour(Blockly.Blocks.keypad.HUE);
     this.appendDummyInput()
-        .appendField(Blockly.Msg.KEYPAD_INIT_PORT)
-        .appendField(new Blockly.FieldDropdown(
-            Blockly.Arduino.Boards.selected.full_ports), 'PORT');
+      .appendField(Blockly.Msg.KEYPAD_INIT_PORT)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.full_ports),
+        "PORT"
+      )
+      .appendField(Blockly.Msg.KEY_R0)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "R0"
+      )
+      .appendField(Blockly.Msg.KEY_R1)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "R1"
+      )
+      .appendField(Blockly.Msg.KEY_R2)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "R2"
+      );
+    this.appendDummyInput()
+      .appendField(Blockly.Msg.KEY_R3)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "R3"
+      )
+      .appendField(Blockly.Msg.KEY_C0)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "C0"
+      )
+      .appendField(Blockly.Msg.KEY_C1)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "C1"
+      )
+      .appendField(Blockly.Msg.KEY_C2)
+      .appendField(
+        new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
+        "C2"
+      );
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip(Blockly.Msg.KEYPAD_TTL);
   },
-  
 };
 
-
-
-Blockly.Blocks['keypad_getKey'] = {
- 
-  init: function() {
-     this.setColour(Blockly.Blocks.keypad.HUE);
-     this.appendDummyInput()
-         .appendField(Blockly.Msg.KEYPAD_READ)
-     this.setOutput(true, Blockly.Types.CHARACTER.output);
-     this.setTooltip(Blockly.Msg.KEYPAD_READ_TTL);
-   },
-   /** @return {!string} The type of return value for the block, an integer. */
-   getBlockType: function() {
-     return Blockly.Types.CHARACTER;
-   },
-   
-  };
-
+Blockly.Blocks["keypad_getKey"] = {
+  init: function () {
+    this.setColour(Blockly.Blocks.keypad.HUE);
+    this.appendDummyInput().appendField(Blockly.Msg.KEYPAD_READ);
+    this.setOutput(true, Blockly.Types.CHARACTER.output);
+    this.setTooltip(Blockly.Msg.KEYPAD_READ_TTL);
+  },
+  /** @return {!string} The type of return value for the block, an integer. */
+  getBlockType: function () {
+    return Blockly.Types.CHARACTER;
+  },
+};
--- blockly/generators/bluepill/Keypad.js
+++ blockly/generators/bluepill/Keypad.js
@@ -1,42 +1,69 @@
-
 /**
  * @fileoverview Code generator for Keypad in HAL layer
- *    
+ *
  */
-'use strict';
+"use strict";
 
-goog.provide('Blockly.Arduino.keypad');
+goog.provide("Blockly.Arduino.keypad");
 
-goog.require('Blockly.Arduino');
+goog.require("Blockly.Arduino");
 
+Blockly.Arduino["keypad_init"] = function (block) {
+  var port = block.getFieldValue("PORT");
+  var gpio = port.charAt(4);
 
+  var R0 = "PIN_" + block.getFieldValue("R0").charAt(2);
+  var R1 = "PIN_" + block.getFieldValue("R1").charAt(2);
+  var R2 = "PIN_" + block.getFieldValue("R2").charAt(2);
+  var R3 = "PIN_" + block.getFieldValue("R3").charAt(2);
+  var C0 = "PIN_" + block.getFieldValue("C0").charAt(2);
+  var C1 = "PIN_" + block.getFieldValue("C1").charAt(2);
+  var C2 = "PIN_" + block.getFieldValue("C2").charAt(2);
 
+  for (var i = 0; i < 4; i++) {
+    var pin = block.getFieldValue("R" + i);
+    Blockly.Arduino.reservePin(
+      block,
+      pin,
+      Blockly.Arduino.PinTypes.INPUT,
+      "keypad pins"
+    );
+  }
+  for (var i = 0; i < 3; i++) {
+    var pin = block.getFieldValue("C" + i);
+    Blockly.Arduino.reservePin(
+      block,
+      pin,
+      Blockly.Arduino.PinTypes.INPUT,
+      "keypad pins"
+    );
+  }
 
+  var pinMainCode =
+    "\nkeypad_t keypad = {" +
+    R0 +
+    "," +
+    R1 +
+    "," +
+    R2 +
+    "," +
+    R3 +
+    "," +
+    C0 +
+    "," +
+    C1 +
+    "," +
+    C2 +
+    "," +
+    port +
+    "};\nKeypad_init(&keypad);\n ";
+  Blockly.Arduino.addMain("keypad_" + port, pinMainCode, false);
 
-Blockly.Arduino['keypad_init'] = function(block) {
-  var port = block.getFieldValue('PORT');
-  var gpio = port.charAt(4); 
-  for(var i =0 ;i<7 ; i++){
-     var pin = gpio + i;
-     Blockly.Arduino.reservePin(
-        block, pin, Blockly.Arduino.PinTypes.INPUT, 'keypad pins');
-     }
-     
-     editFileAtLine("STMCubeProject/HAL/Keypad/Keypad.h", 14, "#define Keypad_PORT     "+port);
-     
-
-  var pinMainCode = '\nKeypad_init();';
-  Blockly.Arduino.addMain('keypad_' + port, pinMainCode, false);
-  
   return "";
-
 };
 
- Blockly.Arduino['keypad_getKey'] = function(block) {
-  
-   var pinMainCode = 'Keypad_Get_Key()';
+Blockly.Arduino["keypad_getKey"] = function (block) {
+  var pinMainCode = "Keypad_Get_Key()";
 
   return [pinMainCode, Blockly.Arduino.ORDER_ATOMIC];
- };
-
-
+};
