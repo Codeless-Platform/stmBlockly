c0b30cf739d803a272150907070a9fb8fbcfd3b2
alaa wahba <alaawahbaa13@gmail.com>
2024-03-07 20:47:15 +0200

    fixing LCD bug

--- blockly/blocks/bluepill/LCD.js
+++ blockly/blocks/bluepill/LCD.js
@@ -1,58 +1,59 @@
-"use strict";
+'use strict';
 
-goog.provide("Blockly.Blocks.lcd");
+goog.provide('Blockly.Blocks.lcd');
 
-goog.require("Blockly.Blocks");
-goog.require("Blockly.Types");
+goog.require('Blockly.Blocks');
+goog.require('Blockly.Types');
 
 /** Common HSV hue for all blocks in this category. */
 Blockly.Blocks.lcd.HUE = 400;
 
-Blockly.Blocks["lcd_init"] = {
+Blockly.Blocks['lcd_init'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
     this.appendDummyInput()
       .appendField(Blockly.Msg.LCD_INIT)
       .appendField(Blockly.Msg.LCD_SIZE)
       .appendField(
-        new Blockly.FieldDropdown([["2x16"], ["4x16"], ["4x20"]]),
-        "SIZE"
+        new Blockly.FieldDropdown([['2x16'], ['4x16'], ['4x20']]),
+        'SIZE'
       )
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.full_ports),
-        "PORT"
+        'PORT'
       )
       .appendField(Blockly.Msg.LCD_D4)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "d4"
+        'd4'
       )
       .appendField(Blockly.Msg.LCD_D5)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "d5"
+        'd5'
       )
       .appendField(Blockly.Msg.LCD_D6)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "d6"
+        'd6'
       );
     this.appendDummyInput()
       .appendField(Blockly.Msg.LCD_D7)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "d7"
+        'd7'
       )
       .appendField(Blockly.Msg.LCD_EN)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "EN"
+        'EN'
       )
       .appendField(Blockly.Msg.LCD_RS)
       .appendField(
         new Blockly.FieldDropdown(Blockly.Arduino.Boards.selected.digitalPins),
-        "RS"
+        'RS'
       );
+    this.setFieldValue('2x16', 'SIZE');
 
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -61,10 +62,10 @@ Blockly.Blocks["lcd_init"] = {
 };
 
 // send char
-Blockly.Blocks["lcd_sendChar"] = {
+Blockly.Blocks['lcd_sendChar'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendValueInput("DATA").appendField(Blockly.Msg.LCD_CHAR);
+    this.appendValueInput('DATA').appendField(Blockly.Msg.LCD_CHAR);
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -73,10 +74,10 @@ Blockly.Blocks["lcd_sendChar"] = {
 };
 
 // send string
-Blockly.Blocks["lcd_sendString"] = {
+Blockly.Blocks['lcd_sendString'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendValueInput("DATA")
+    this.appendValueInput('DATA')
       .setCheck(Blockly.Types.TEXT.checkList)
       .appendField(Blockly.Msg.LCD_STRING);
     this.setInputsInline(true);
@@ -86,10 +87,10 @@ Blockly.Blocks["lcd_sendString"] = {
   },
 };
 // send Number
-Blockly.Blocks["lcd_sendNumber"] = {
+Blockly.Blocks['lcd_sendNumber'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendValueInput("DATA")
+    this.appendValueInput('DATA')
       .setCheck(Blockly.Types.NUMBER.checkList)
       .appendField(Blockly.Msg.LCD_NUMBER);
     this.setInputsInline(true);
@@ -99,7 +100,7 @@ Blockly.Blocks["lcd_sendNumber"] = {
   },
 };
 // clear screen
-Blockly.Blocks["lcd_clear"] = {
+Blockly.Blocks['lcd_clear'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
     this.appendDummyInput().appendField(Blockly.Msg.LCD_CLEAR);
@@ -109,14 +110,14 @@ Blockly.Blocks["lcd_clear"] = {
 };
 
 // go to x y
-Blockly.Blocks["lcd_goto"] = {
+Blockly.Blocks['lcd_goto'] = {
   init: function () {
     this.setColour(Blockly.Blocks.lcd.HUE);
-    this.appendValueInput("ROW")
+    this.appendValueInput('ROW')
       .setCheck(Blockly.Types.NUMBER.checkList)
       .appendField(Blockly.Msg.LCD_GOTOx);
     this.appendDummyInput().appendField(Blockly.Msg.LCD_GOTOy);
-    this.appendValueInput("COL").setCheck(Blockly.Types.NUMBER.checkList);
+    this.appendValueInput('COL').setCheck(Blockly.Types.NUMBER.checkList);
     this.setInputsInline(true);
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
