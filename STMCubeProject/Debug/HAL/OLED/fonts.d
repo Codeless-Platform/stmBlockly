27962312379ed164cbeaea1ad8d68b6f405a471a
alaa wahba <alaawahbaa13@gmail.com>
2024-04-20 20:34:59 +0200

    cleaning the project

--- STMCubeProject/Debug/HAL/OLED/fonts.d
+++
@@ -1,8 +0,0 @@
-HAL/OLED/fonts.o: ../HAL/OLED/fonts.c ../HAL/OLED/fonts.h \
- ../HAL/OLED/../../MCAL/Lib/STM32_F103x6.h \
- ../HAL/OLED/../../MCAL/Lib/Platform_types.h \
- ../HAL/OLED/../../MCAL/Lib/BIT_MATH.h
-../HAL/OLED/fonts.h:
-../HAL/OLED/../../MCAL/Lib/STM32_F103x6.h:
-../HAL/OLED/../../MCAL/Lib/Platform_types.h:
-../HAL/OLED/../../MCAL/Lib/BIT_MATH.h:
